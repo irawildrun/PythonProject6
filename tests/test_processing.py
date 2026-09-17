ab5ff31617dc626fb0f195db4521910a27f22079
Irina Chuvashova <irishkachuv@gmail.com>
2026-09-18 01:04:19 +0300

    fixed tests by linters

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,8 +1,10 @@
+from typing import Any, Dict, List
 import pytest
 from src.processing import filter_by_state, sort_by_date
 
+
 @pytest.fixture
-def operations():
+def operations() -> List[Dict[str, Any]]:
     return [
         {'id': 1, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
         {'id': 2, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
@@ -13,36 +15,38 @@ def operations():
 
 # поскольку вывод данных по фильтрам громоздкий, будем проверять только по id
 # проверка работы функции filter_by_state и проверка на несуществующий статус
-@pytest.mark.parametrize('state, expected_ids', [
-    ('EXECUTED', [1, 2]),
-    ('CANCELED', [3, 4]),
-    ('UNKNOWN', []),
-])
-def test_filter_by_state(operations, state, expected_ids):
+@pytest.mark.parametrize(
+    'state, expected_ids',
+    [
+        ('EXECUTED', [1, 2]),
+        ('CANCELED', [3, 4]),
+        ('UNKNOWN', []),
+    ],
+)
+def test_filter_by_state(operations: List[Dict[str, Any]], state: str, expected_ids: List[int]) -> None:
     assert [operation['id'] for operation in filter_by_state(operations, state)] == expected_ids
 
 
 # проверка, что при отсутствии входных данных на статус, выводятся значения по 'EXECUTED'
-def test_filter_by_state_default(operations):
+def test_filter_by_state_default(operations: List[Dict[str, Any]]) -> None:
     assert [operation['id'] for operation in filter_by_state(operations)] == [1, 2]
 
 
 # проверка на пустой входной список операций
-def test_filter_by_state_empty_list():
+def test_filter_by_state_empty_list() -> None:
     assert filter_by_state([], 'EXECUTED') == []
 
 
 # проверка, что при отсутствии входных данных на направление времени, выводятся значения по-убыванию
-def test_sort_by_date_default(operations):
+def test_sort_by_date_default(operations: List[Dict[str, Any]]) -> None:
     assert [operation['id'] for operation in sort_by_date(operations)] == [1, 4, 3, 2]
 
 
 # проверка направления по reverse
-def test_sort_by_date_reverse(operations):
+def test_sort_by_date_reverse(operations: List[Dict[str, Any]]) -> None:
     assert [operation['id'] for operation in sort_by_date(operations, reverse=False)] == [2, 3, 4, 1]
 
 
 # проверка на пустой входной список операций
-def test_sort_by_date_empty_list():
+def test_sort_by_date_empty_list() -> None:
     assert sort_by_date([]) == []
-
